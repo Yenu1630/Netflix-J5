f9d25a37fb638c15180fe1c394cdf27258244a92
unknown <yenu1630@gmail.com>
2022-02-16 00:25:30 -0600

    Updates

---
+++ precache-manifest.a09c13acbae0840112324b9e6c1202f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43322b09b4a6e4c7075358ff912878c",
+    "url": "/netflix-j5/index.html"
+  },
+  {
+    "revision": "8687f5b41c611c3b99f1",
+    "url": "/netflix-j5/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "9fd1e930ff2339a5d73d",
+    "url": "/netflix-j5/static/js/2.2e428b8c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-j5/static/js/2.2e428b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8687f5b41c611c3b99f1",
+    "url": "/netflix-j5/static/js/main.9cb05e02.chunk.js"
+  },
+  {
+    "revision": "95b5f399533b73b5d830",
+    "url": "/netflix-j5/static/js/runtime-main.e987b2a5.js"
+  }
+]);
