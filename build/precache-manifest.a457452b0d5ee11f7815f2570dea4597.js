1008299e2f9a5375fd5d6d23496baa89225a881c
unknown <yenu1630@gmail.com>
2022-02-16 00:19:52 -0600

    first commit

---
+++ build/precache-manifest.a457452b0d5ee11f7815f2570dea4597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c1d60e12e16439f2a45e149209f374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4579c5fcc405cc3abb4",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4579c5fcc405cc3abb4",
+    "url": "/static/js/main.a8ba9598.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
